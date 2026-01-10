57ee528133943169d57772dc8f1d93c3261d017e
pknot212121 <pknot@student.agh.edu.pl>
2026-01-10 15:41:01 +0100

    Add error handling to wrapper

--- shader.cpp
+++ shader.cpp
@@ -43,55 +43,109 @@ void Shader::SetFloat(const char *name, float value, bool useShader)
 {
     if (useShader)
         this->Use();
-    glUniform1f(glGetUniformLocation(this->ID, name), value);
+    int location = glGetUniformLocation(this->ID,name);
+    if (location == -1)
+    {
+        std::cerr << "NO SUCH UNIFORM FOUND IN THIS SHADER: " << name << std::endl;
+        exit(1);
+    }
+    glUniform1f(location, value);
 }
 void Shader::SetInteger(const char *name, int value, bool useShader)
 {
     if (useShader)
         this->Use();
-    glUniform1i(glGetUniformLocation(this->ID, name), value);
+    int location = glGetUniformLocation(this->ID,name);
+    if (location == -1)
+    {
+        std::cerr << "NO SUCH UNIFORM FOUND IN THIS SHADER: " << name << std::endl;
+        exit(1);
+    }
+    glUniform1i(location, value);
 }
 void Shader::SetVector2f(const char *name, float x, float y, bool useShader)
 {
     if (useShader)
         this->Use();
-    glUniform2f(glGetUniformLocation(this->ID, name), x, y);
+    int location = glGetUniformLocation(this->ID,name);
+    if (location == -1)
+    {
+        std::cerr << "NO SUCH UNIFORM FOUND IN THIS SHADER: " << name << std::endl;
+        exit(1);
+    }
+    glUniform2f(location, x, y);
 }
 void Shader::SetVector2f(const char *name, const glm::vec2 &value, bool useShader)
 {
     if (useShader)
         this->Use();
-    glUniform2f(glGetUniformLocation(this->ID, name), value.x, value.y);
+    int location = glGetUniformLocation(this->ID,name);
+    if (location == -1)
+    {
+        std::cerr << "NO SUCH UNIFORM FOUND IN THIS SHADER: " << name << std::endl;
+        exit(1);
+    }
+    glUniform2f(location, value.x, value.y);
 }
 void Shader::SetVector3f(const char *name, float x, float y, float z, bool useShader)
 {
     if (useShader)
         this->Use();
-    glUniform3f(glGetUniformLocation(this->ID, name), x, y, z);
+    int location = glGetUniformLocation(this->ID,name);
+    if (location == -1)
+    {
+        std::cerr << "NO SUCH UNIFORM FOUND IN THIS SHADER: " << name << std::endl;
+        exit(1);
+    }
+    glUniform3f(location, x, y, z);
 }
 void Shader::SetVector3f(const char *name, const glm::vec3 &value, bool useShader)
 {
     if (useShader)
         this->Use();
-    glUniform3f(glGetUniformLocation(this->ID, name), value.x, value.y, value.z);
+    int location = glGetUniformLocation(this->ID,name);
+    if (location == -1)
+    {
+        std::cerr << "NO SUCH UNIFORM FOUND IN THIS SHADER: " << name << std::endl;
+        exit(1);
+    }
+    glUniform3f(location, value.x, value.y, value.z);
 }
 void Shader::SetVector4f(const char *name, float x, float y, float z, float w, bool useShader)
 {
     if (useShader)
         this->Use();
-    glUniform4f(glGetUniformLocation(this->ID, name), x, y, z, w);
+    int location = glGetUniformLocation(this->ID,name);
+    if (location == -1)
+    {
+        std::cerr << "NO SUCH UNIFORM FOUND IN THIS SHADER: " << name << std::endl;
+        exit(1);
+    }
+    glUniform4f(location, x, y, z, w);
 }
 void Shader::SetVector4f(const char *name, const glm::vec4 &value, bool useShader)
 {
     if (useShader)
         this->Use();
-    glUniform4f(glGetUniformLocation(this->ID, name), value.x, value.y, value.z, value.w);
+    int location = glGetUniformLocation(this->ID,name);
+    if (location == -1)
+    {
+        std::cerr << "NO SUCH UNIFORM FOUND IN THIS SHADER: " << name << std::endl;
+        exit(1);
+    }
+    glUniform4f(location, value.x, value.y, value.z, value.w);
 }
 void Shader::SetMatrix4(const char *name, const glm::mat4 &matrix, bool useShader)
 {
     if (useShader)
         this->Use();
-    glUniformMatrix4fv(glGetUniformLocation(this->ID, name), 1, false, glm::value_ptr(matrix));
+    int location = glGetUniformLocation(this->ID,name);
+    if (location == -1)
+    {
+        std::cerr << "NO SUCH UNIFORM FOUND IN THIS SHADER: " << name << std::endl;
+        exit(1);
+    }
+    glUniformMatrix4fv(location, 1, false, glm::value_ptr(matrix));
 }
 
 
